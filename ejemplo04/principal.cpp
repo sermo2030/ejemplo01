6e26345f79045ec8a17848da6113a005845e8976
sermo2030 <sermoni050607@gmail.com>
2021-09-09 13:59:08 -0400

    mis datos

--- ejemplo04/principal.cpp
+++ ejemplo04/principal.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 int main(){
 
-	cout << "hola mundo";
-	cout << "hola mundo";
-	cout << "hola mundo";
-	cout << "hola mundo";
+	cout << "Hola mi nombre es: ";
+	cout << "Sergio Alejandro Huarachi Navarro\n";
+	cout << "Carrera : Ciencias de la Computacion\n";
+	cout << "Telefono : 68627565\n";
+	cout << "Correo : sermoni050607@gmail.com\n";
 }
